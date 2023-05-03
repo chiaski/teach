ba23ac6f1d896e5a22d0c4688691be12aaaee484
Chia Amisola <chiamisola@gmail.com>
2023-05-02 23:43:03 -0700

    ~

--- ambient.institute/institute.js
+++ ambient.institute/institute.js
@@ -22,3 +22,31 @@ $(".institute-index .wrapper a").on("mouseover", function(){
   $(".institute-map .room[name='" + n +"']").attr("active", "");
   
 });
+
+// https://opensheet.elk.sh/1duW1V3VfMU92-zHApZnCZvpSNy9wyM79kFk95zrxJ18/1
+
+
+$(document).ready(function () {
+
+  $.getJSON("https://opensheet.elk.sh/1duW1V3VfMU92-zHApZnCZvpSNy9wyM79kFk95zrxJ18/1", function (data) {
+
+    data.forEach(function (row, i) {
+      
+      console.log(row);
+      
+      $(`<a href="` + row.link +`" link>
+        <div class="row">
+          <span type>` + row.type +`</span>
+          <span name>` + row.name +`</span>
+          <span desc>` + row.desc +`</span>
+          <span keywords>` + row.keywords +`</span>
+          <span year>` + row.year +`</span>
+        </div>
+        </a>`)
+        .appendTo("#index .wrapper");
+      
+    })
+    
+  })
+  
+});
